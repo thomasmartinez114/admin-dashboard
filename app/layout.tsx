ad23ed08bffc1c1c8ef72aae3b994969412c9581
Thomas Martinez <devtommie114@gmail.com>
2025-05-20 23:39:55 -0400

    sidebar command

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,7 +30,12 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        {children}
+        <div className='flex'>
+          <div className='hidden md:block h-[100vh] w-[300px]'>
+            <Sidebar />
+          </div>
+          <div className='p-5 w-full -md:max-w-[1140px]'>{children}</div>
+        </div>
       </body>
     </html>
   );
